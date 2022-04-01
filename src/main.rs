c9b14700ab6a2aec63cfed5f389e0beb81511603
Vilém Pavlíček <pavlicek@cngroup.dk>
2022-04-01 12:04:57 +0200

    removed what wasn't supposed to be here

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,3 @@
-mod grpc;
-mod logger;
-
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let _guard = logger::init();
-
-    tracing::debug!(
-        name = env!("CARGO_PKG_NAME"),
-        version = env!("CARGO_PKG_VERSION"),
-        commit = env!("GIT_HASH"),
-        "App started",
-    );
-
-    grpc::start_server().await;
-    Ok(())
+fn main() {
+    println!("Hello, world!");
 }
