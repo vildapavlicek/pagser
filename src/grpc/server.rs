7bf0cc918a5bae7f852c33041908c92955c4abda
Vilém Pavlíček <pavlicek@cngroup.dk>
2022-06-16 14:45:23 +0200

    implemented + post written

--- src/grpc/server.rs
+++ src/grpc/server.rs
@@ -1,6 +1,11 @@
 use super::{customer_service::CustomerService, customer_service_server::CustomerServiceServer};
-use std::net::SocketAddr;
-use tonic::transport::Server;
+use std::{net::SocketAddr, time::Duration};
+use tonic::{
+    body::BoxBody,
+    transport::{Body, Server},
+};
+use tower_http::classify::GrpcFailureClass;
+use tracing::{debug, error, info, Span};
 
 pub async fn run() {
     let addr = std::env::var("PAGSER_ADDR")
@@ -8,7 +13,22 @@ pub async fn run() {
         .parse::<SocketAddr>()
         .expect("failed parse address to bind to");
 
+    let tracing_layer = tower::ServiceBuilder::new().layer(
+        tower_http::trace::TraceLayer::new_for_grpc()
+            .make_span_with(|_request: &http::Request<Body>| {
+                tracing::info_span!("grpc_request", uuid = %uuid::Uuid::new_v4().to_string())
+            })
+            .on_request(|request: &http::Request<Body>, _span: &Span| info!(path = %request.uri().path(), "received new request"))
+            .on_response(|_response: &http::Response<BoxBody>, latency: Duration, _span: &Span| {
+                debug!(?latency, "generated response")
+            })
+            .on_failure(|failure_class: GrpcFailureClass, latency: Duration, _span: &Span| {
+                error!(?latency, ?failure_class, "generated response with error code")
+            })
+    ).into_inner();
+
     Server::builder()
+        .layer(tracing_layer)
         .add_service(CustomerServiceServer::new(CustomerService))
         .serve(addr)
         .await
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
+use tracing::info;
+
 mod grpc;
+mod logger;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    println!("Hello, world!");
+    let _guard = logger::init();
+    info!(
+        version = env!("CARGO_PKG_VERSION"),
+        git_hash = env!("GIT_HASH"),
+        "Starting server..."
+    );
+
     grpc::server::run().await;
     Ok(())
 }
